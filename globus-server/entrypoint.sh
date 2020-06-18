a84bad5ac5a29afcb83765f2bb3783e6f2edc961
Marshall McDonnell <mcdonnellmt@ornl.gov>
2020-06-18 18:34:05 -0400

    Update entrypoint.sh

--- globus-server/entrypoint.sh
+++ globus-server/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#
-# Entry point to start a Globus Connect Server
-# Requires that the following en vars are set:
-#
 export HOSTNAME=`curl http://169.254.169.254/latest/meta-data/public-hostname`
 export SHORT_HOSTNAME=`echo ${HOSTNAME%%.*}`
 
